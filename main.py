70e4a4269aa5474dad8af72a869e2fb0b5d5e044
Egorov <Oleg.Egorov@pepsico.com>
2021-09-03 01:48:21 +0300

    new commit

--- main.py
+++ main.py
@@ -26,8 +26,14 @@ from telegram.ext import (
 )
 
 # Enable logging
+# logging.basicConfig(filename='logfile.log',
+#                     filemode='a',
+#                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+#                     level=logging.INFO
+# )
 logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO
 )
 
 logger = logging.getLogger(__name__)
@@ -37,12 +43,12 @@ GENDER, PHOTO, LOCATION, BIO = range(4)
 
 def start(update: Update, context: CallbackContext) -> int:
     """Starts the conversation and asks the user about their gender."""
-    reply_keyboard = [['Boy', 'Girl', 'Other']]
+    reply_keyboard = [['Лёша', 'Олексейко', 'Тростников']]
 
     update.message.reply_text(
-        'Hi! My name is Professor Bot. I will hold a conversation with you. '
-        'Send /cancel to stop talking to me.\n\n'
-        'Are you a boy or a girl?',
+        'Привет! Я твой тестировочный бот. '
+        'Нажми /cancel чтобы я замолчал.\n\n'
+        'А какой ты сегодня?',
         reply_markup=ReplyKeyboardMarkup(
             reply_keyboard, one_time_keyboard=True, input_field_placeholder='Boy or Girl?'
         ),
@@ -56,8 +62,8 @@ def gender(update: Update, context: CallbackContext) -> int:
     user = update.message.from_user
     logger.info("Gender of %s: %s", user.first_name, update.message.text)
     update.message.reply_text(
-        'I see! Please send me a photo of yourself, '
-        'so I know what you look like, or send /skip if you don\'t want to.',
+        'Ого! Вышли мне своё фото, хочу посмотреть, как ты выглядишь. '
+        'или нажми /skip , если не хочешь.',
         reply_markup=ReplyKeyboardRemove(),
     )
 
@@ -68,21 +74,20 @@ def photo(update: Update, context: CallbackContext) -> int:
     """Stores the photo and asks for a location."""
     user = update.message.from_user
     photo_file = update.message.photo[-1].get_file()
-    photo_file.download('user_photo.jpg')
-    logger.info("Photo of %s: %s", user.first_name, 'user_photo.jpg')
+    photo_file.download(f'media/download/{user.first_name}.jpg')
+    logger.info("Photo of %s: %s", user.first_name, f'{user.first_name}.jpg')
     update.message.reply_text(
-        'Gorgeous! Now, send me your location please, or send /skip if you don\'t want to.'
+        'Красивое! А теперь пришли мне свою Геолокацию. Или нажми /skip , если не хочешь.'
     )
 
     return LOCATION
-
 
 def skip_photo(update: Update, context: CallbackContext) -> int:
     """Skips the photo and asks for a location."""
     user = update.message.from_user
     logger.info("User %s did not send a photo.", user.first_name)
     update.message.reply_text(
-        'I bet you look great! Now, send me your location please, or send /skip.'
+        'Уверен, что ты выглядишь замечательно! А теперь пришли мне свою Геолокацию. Или нажми /skip , если не хочешь.'
     )
 
     return LOCATION
@@ -96,7 +101,7 @@ def location(update: Update, context: CallbackContext) -> int:
         "Location of %s: %f / %f", user.first_name, user_location.latitude, user_location.longitude
     )
     update.message.reply_text(
-        'Maybe I can visit you sometime! At last, tell me something about yourself.'
+        'Так мне проще будет найти тебя! Теперь расскажи мне о себе.'
     )
 
     return BIO
@@ -107,7 +112,7 @@ def skip_location(update: Update, context: CallbackContext) -> int:
     user = update.message.from_user
     logger.info("User %s did not send a location.", user.first_name)
     update.message.reply_text(
-        'You seem a bit paranoid! At last, tell me something about yourself.'
+        'Да ты чего? Не бойся! Хотя бы расскажи мне что-нибудь о себе.'
     )
 
     return BIO
@@ -117,7 +122,7 @@ def bio(update: Update, context: CallbackContext) -> int:
     """Stores the info about the user and ends the conversation."""
     user = update.message.from_user
     logger.info("Bio of %s: %s", user.first_name, update.message.text)
-    update.message.reply_text('Thank you! I hope we can talk again some day.')
+    update.message.reply_text('Спасибо! Надеюсь, мы еще свяжемся. Нажми /start , чтобы начать снова.')
 
     return ConversationHandler.END
 
@@ -127,7 +132,7 @@ def cancel(update: Update, context: CallbackContext) -> int:
     user = update.message.from_user
     logger.info("User %s canceled the conversation.", user.first_name)
     update.message.reply_text(
-        'Bye! I hope we can talk again some day.', reply_markup=ReplyKeyboardRemove()
+        'Пока! Надеюсь, мы еще свяжемся. Нажми /start , чтобы начать снова.', reply_markup=ReplyKeyboardRemove()
     )
 
     return ConversationHandler.END
@@ -145,7 +150,7 @@ def main() -> None:
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler('start', start)],
         states={
-            GENDER: [MessageHandler(Filters.regex('^(Boy|Girl|Other)$'), gender)],
+            GENDER: [MessageHandler(Filters.regex('^(Лёша|Олексейко|Тростников)$'), gender)],
             PHOTO: [MessageHandler(Filters.photo, photo), CommandHandler('skip', skip_photo)],
             LOCATION: [
                 MessageHandler(Filters.location, location),
